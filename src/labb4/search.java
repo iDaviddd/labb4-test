fae6e61bd2fe83985ab3a47cd4c4ca1e1ad4652b
iDaviddd <david@ohrbom.se>
2020-11-30 16:17:05 +0100

    First commit

---
+++ src/labb4/search.java
@@ -0,0 +1,40 @@
+package labb4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class search {
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Wrong arguments!");
+			return;
+		}
+
+		String pattern = args[0];
+		String filePath = args[1];
+
+		ArrayList<String> lines = new ArrayList<String>();
+
+		try {
+			Scanner scanner = new Scanner(new File(filePath));
+
+			while (scanner.hasNextLine()) {
+				lines.add(scanner.nextLine());
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			return;
+		}
+
+		for (String line : lines) {
+			if (line.contains(pattern)) {
+				System.out.println(line);
+			}
+		}
+	}
+
+}
